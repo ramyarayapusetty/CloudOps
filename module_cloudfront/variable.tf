95038867409044675c12190fe5f5e354f1b5f8e3
ramyarayapusetty <137818429+ramyarayapusetty@users.noreply.github.com>
2024-06-13 17:57:51 +0530

    Update variable.tf

--- module_cloudfront/variable.tf
+++ module_cloudfront/variable.tf
@@ -1,37 +1,31 @@
 variable "region" {
   description = "Region in which you want to create distribution"
   type = string
-  default = "us-west-2"
 }
 
 variable "bucket" {
     description = "the name of the bucket you are associating with cloudfront"
     type = string
-    default = "ramyacloudfront26"
 }
 
 variable "acl" {
     description = "the properties of acl"
     type = string
-    default = "private"
 }
 
 variable "s3_origin_id" {
     description = "the origin id of the creating s3 bucket"
     type = string
-    default = "myS3Origin"
 }
 
 variable "origin_access_control_id" {
   description = "the origin access control id"
   type = string
-  default = "E34MVPD82SVCX8"
 }
 
 variable "enabled" {
     description = "contains whether it is enabled or not"
     type = bool
-    default = true
 }
 
 variable "is_ipv6_enabled" {
@@ -43,13 +37,11 @@ variable "is_ipv6_enabled" {
 variable "comment" {
     description = "it contains comments to describe"
     type = string
-    default = "some comments"
 }
 
 variable "default_root_object" {
     description = "it contains some file"
     type = string
-    default = "index.html"
 }
 
 variable "include_cookies" {
@@ -61,84 +53,86 @@ variable "include_cookies" {
 variable "prefix" {
     description = "the prefix of the cloudfront"
     type = string
-    default = "my prefix"
 }
 
 variable "allowed_methods" {
     description = "the allowed methods in cloudfront"
     type = list(string)
-    default = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
 }
 
 variable "cached_methods" {
     description = "the cached methods in cloudfront"
     type = list(string)
-    default = ["GET", "HEAD"]
 }
 
 variable "query_string" {
     description = "conatains true or false"
     type = bool
-    default = false
 }
 
 variable "forward" {
     description = "contains whether cookies or forwaded or not"
     type = string
-    default = "none"
 }
 
 variable "price_class" {
     description = " The price class determines the price tier (edge locations) that CloudFront will use for serving your content."
     type = string
-    default = "PriceClass_200"
 }
 
 variable "restriction_type" {
     description = "the type of restriction for the cloudfront"
     type = string
-    default = "whitelist"
 }
 
 variable "locations" {
     description = "the countries you want to blacklist or whitelist"
     type = list(string)
-    default = [ "US", "CA", "GB", "DE" ]
 }
 
 variable "Environment" {
     description = "the environment of the distribution"
     type = string
-    default = "production"
 }
 
 variable "cloudfront_default_certificate" {
     description = "the certification of cloudfront"
     type = bool
-    default = true
 }
 
 variable "min_ttl" {
     description = "the minimum value"
     type = number
-    default = 0
 }
  variable "default_ttl" {
     description = "the default value"
     type = number
-    default = 3600
  }
 
  variable "max_ttl" {
     description = "the maximum value"
-    type = number
-    default = 86400    
+    type = number   
 }
 
 variable "viewer_protocol_policy" {
     description = "the viewerpolicy"
     type = string
-    default = "allow-all"
+}
+variable "team" {
+  description = "Team name"
+  type = string
+}
+variable "environment" {
+  description = "Environment name"
+  type = string
+}
+variable "owner" {
+  description = "Owner name"
+  type = string
+}
+variable "owner_email" {
+  description = "Owner email"
+  type = string
 }
 
 variable "Name" {
