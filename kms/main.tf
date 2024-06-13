7afe74cd6cb806878d3a6b46e62280cba71a6d80
ramyarayapusetty <137818429+ramyarayapusetty@users.noreply.github.com>
2024-06-13 17:16:56 +0530

    Update main.tf

--- kms/main.tf
+++ kms/main.tf
@@ -3,4 +3,11 @@ resource "aws_kms_key" "a" {}
 resource "aws_kms_alias" "a" {
   name          = var.name
   target_key_id = aws_kms_key.a.key_id
+  tags={
+      team=var.team
+      environment=var.environment
+      owner=var.owner
+      owner_email=var.owner_email
+      creation_date=timestamp()
+  }
 }
