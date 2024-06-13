dcbd2d7984a9e79042748597ab8dcb99e7ea8300
ramyarayapusetty <137818429+ramyarayapusetty@users.noreply.github.com>
2024-06-13 20:21:05 +0530

    Update variable.tf

--- module_elb/variable.tf
+++ module_elb/variable.tf
@@ -1,47 +1,81 @@
 variable "region" {
   description = "the region where you want to create elb"
   type = string
-  default = "us-east-1"
 }
 
 variable "from_port" {
     description = "the starting port range"
     type = number
-    default = 80
 }
 
 variable "to_port" {
     description = "the ending port range"
     type = number
-    default = 80
 }
 
-variable "protocol" {
+variable "aws_lb_target_group_protocol" {
     description = "the http protocol"
     type = string
-    default = "tcp"
 }
 
 variable "name" {
     description = "the name of the elb"
     type = string
-    default = "my-nlb"
 }
 
 variable "internal" {
     description = "value"
     type = bool
-    default = false
 }
 
 variable "load_balancer_type" {
     description = "the type of the load balancer"
     type = string
-    default = "network"
 }
 
 variable "target_id" {
     description = "the id of any target"
     type = string
-    default = "i-0bdf69a0c558061f8"
+}
+
+variable "aws_vpc_cidr_block" {
+    description = "the ip address of the vpc cider block"
+    type = string
+}
+
+variable "aws_subnet_cider_block" {
+    description = "the ip address of the subnet cider block"
+    type = string
+}
+
+variable "ingress_cidr_block" {
+    description = "the ip address of the ingress cidr block"
+    type = list(string)
+}
+
+variable "aws_lb_target_group_name" {
+    description = "the name of the load balancer target group name"
+    type = string
+}
+
+variable "protocol" {
+    description = "th eprotocol name"
+    type = string
+}
+
+variable "team" {
+  description = "Team name"
+  type = string
+}
+variable "environment" {
+  description = "Environment name"
+  type = string
+}
+variable "owner" {
+  description = "Owner name"
+  type = string
+}
+variable "owner_email" {
+  description = "Owner email"
+  type = string
 }
