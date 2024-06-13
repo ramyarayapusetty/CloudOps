6f8e4a75744eb3b65373e412b8f88875dfa57e42
ramyarayapusetty <137818429+ramyarayapusetty@users.noreply.github.com>
2024-06-13 16:34:35 +0530

    Update variables.tf

--- ecr/variables.tf
+++ ecr/variables.tf
@@ -1,26 +1,20 @@
 variable "name" {
     description = "The name of the repository"
     type = string
-    default = "demo-repository1"
 }
-
 variable "team" {
   description = "Team name"
   type = string
-  default = "CloudOps"
 }
 variable "environment" {
   description = "Environment name"
   type = string
-  default = "Practice"
 }
 variable "owner" {
   description = "Owner name"
   type = string
-  default = "Ramya Sri Rayapusetty"
 }
 variable "owner_email" {
   description = "Owner email"
   type = string
-  default = "ramyasrirayapusetty@gmail.com"
 }
