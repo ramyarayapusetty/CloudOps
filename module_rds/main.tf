bdcacefa92e70216cfea2c0de5b9c58c7c980d61
ramyarayapusetty <137818429+ramyarayapusetty@users.noreply.github.com>
2024-06-14 11:09:35 +0530

    Update main.tf

--- module_rds/main.tf
+++ module_rds/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
+  allocated_storage    = var.allocated_storage
   db_name              = var.db_name
   engine               = var.engine
   engine_version       = var.engine_version
@@ -7,4 +7,11 @@ resource "aws_db_instance" "default" {
   username             = var.username
   password             = var.password
   parameter_group_name = var.parameter_group_name
+  tags={
+      team=var.team
+      environment=var.environment
+      owner=var.owner
+      owner_email=var.owner_email
+      creation_date=timestamp()
+  }
 }
