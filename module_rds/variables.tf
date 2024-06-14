a7f81a0489d0d55c55919451849e3d453d2f6b3a
ramyarayapusetty <137818429+ramyarayapusetty@users.noreply.github.com>
2024-06-14 11:10:09 +0530

    Update variables.tf

--- module_rds/variables.tf
+++ module_rds/variables.tf
@@ -1,41 +1,56 @@
 variable "db_name" {
     description = "The custom name of our databse"
     type = string
-    default = "mydb"
 }
 
 variable "engine" {
     description = "The name of the engine"
     type = string
-    default = "mysql"
 }
 
 variable "engine_version" {
     description = "The version of the engine"
     type = string
-    default = "8.0"
 }
 
 variable "instance_class" {
     description = "The instance class where we want to run"
     type = string
-    default = "db.t3.micro"
 }
 
 variable "username" {
     description = "username to access the database"
     type = string
-    default = "db1"
 }
 
 variable "password" {
     description = "password to access the database through the provided"
     type = string
-    default = "password"
 }
 
 variable "parameter_group_name" {
     description = "group name of the parameter"
     type = string
-    default = "default.mysql8.0"
+}
+
+variable "allocated_storage" {
+    description = "the allocated storage to the rds"
+    type = number
+}
+
+variable "team" {
+  description = "Team name"
+  type = string
+}
+variable "environment" {
+  description = "Environment name"
+  type = string
+}
+variable "owner" {
+  description = "Owner name"
+  type = string
+}
+variable "owner_email" {
+  description = "Owner email"
+  type = string
 }
